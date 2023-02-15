27f8f9f2e1ebbe89010c0d0b5d589979212d8f61
tharushaYT <123551512+tharushaYT@users.noreply.github.com>
2023-02-15 09:24:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,15 +14,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94764497078'] //ur owner number
-global.ownername = "Sandaru Nethsara" //ur owner name
-global.ytname = "YT: Sandaru Yt" //ur yt chanel name
+global.owner = ['94764470310 '] //ur owner number
+global.ownername = "𝚙𝚊𝚋𝚊𝚜𝚊𝚛𝚊 𝚖𝚊𝚕𝚒𝚗𝚍𝚊" //ur owner name
+global.ytname = "YT:𝚙𝚊𝚋𝚊𝚜𝚊𝚛𝚊 𝚖𝚊𝚕𝚗𝚍𝚊" //ur yt chanel name
 global.socialm = "GitHub: Mr-Katussa" //ur github or insta name
 global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
 
 //bot bomdy 
-global.ownernomer = "94764497078" //ur number
-global.premium = ['94764497078'] //ur premium number
+global.ownernomer = "94764470310 " //ur number
+global.premium = ['94764470310'] //ur premium number
 global.botname = 'Xcoco' //ur bot name
 global.themeemoji = "♻️" //ur theme emoji
 global.packname = "Sticker By Xcoco" //ur sticker watermark packname
